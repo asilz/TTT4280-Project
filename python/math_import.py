433de934e5fe63e0462f08bb34998493510a3739
Asil Zogby <asilz@stud.ntnu.no>
2025-02-17 13:34:42 +0100

    Fix cross correlation offset error

--- python/math_import.py
+++ python/math_import.py
@@ -5,10 +5,10 @@ def calculate_angle(n31, n21, n32):
 
 
 def find_delay_in_seconds(signal0, signal1, frequency):
-    return (np.argmax(np.abs(np.correlate(signal0,signal1, 'full'))) - len(signal0)) / frequency
+    return (np.argmax(np.abs(np.correlate(signal0,signal1, 'full'))) + 1 - len(signal0)) / frequency
 
 def find_delay_in_samples(signal0, signal1):
-    return np.argmax(np.abs(np.correlate(signal0,signal1, 'full'))) - len(signal0)
+    return np.argmax(np.abs(np.correlate(signal0,signal1, 'full'))) + 1 - len(signal0)
 
 def cross_correlate(signal0, signal1):
     return np.correlate(signal0,signal1, 'full')

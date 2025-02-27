1ceea7276c552a0828a9e2bbfc9d514f8d6f396b
Asil Zogby <asilz@stud.ntnu.no>
2025-02-27 17:58:20 +0100

    Add pulse data processing

--- python/lab3/simple_model.py
+++ python/lab3/simple_model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter
 
 
 muabo = np.genfromtxt("./muabo.txt", delimiter=",")
@@ -47,6 +49,14 @@ def SNR(signal, target_frequency, sampling_frequency):
             return np.abs(fft_y[i])/(np.sum(np.abs(fft_y))-np.abs(fft_y[i]))
     return -1
 
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    return butter(order, [lowcut, highcut], fs=fs, btype='band')
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
     
 #bvf = 1 # Blood volume fraction, average blood amount in tissue
 oxy = 0.8 # Blood oxygenation
@@ -60,10 +70,74 @@ mua001 = get_mua(0.01, oxy, wavelength)
 mua1 = get_mua(1.0, oxy, wavelength)
 musr = get_musr(wavelength)
 
-print(get_contrast(diffusion_equation(300*10**(-6), mua1,musr)/diffusion_equation(0.0, mua1, musr), diffusion_equation(300*10**(-6), mua001,musr)/diffusion_equation(0.0, mua001, musr)))
-print(get_penetration_depth(mua001, musr))
-print(diffusion_equation(0.013, mua001, musr)/diffusion_equation(0.0, mua001,musr))
+#print(get_contrast(diffusion_equation(300*10**(-6), mua1,musr)/diffusion_equation(0.0, mua1, musr), diffusion_equation(300*10**(-6), mua001,musr)/diffusion_equation(0.0, mua001, musr)))
+#print(get_penetration_depth(mua001, musr))
+#print(diffusion_equation(0.013, mua001, musr)/diffusion_equation(0.0, mua001,musr))
 
-x = np.linspace(0,10000)
-sig = np.sin(1*x)
-print(SNR(sig, 2000, 10000))
+fig = plt.figure()
+red_fft_ax = fig.add_subplot(3, 2, 1)
+red_ax = fig.add_subplot(3, 2, 2)
+green_fft_ax = fig.add_subplot(3, 2, 3)
+green_ax = fig.add_subplot(3, 2, 4)
+blue_fft_ax = fig.add_subplot(3, 2, 5)
+blue_ax = fig.add_subplot(3, 2, 6)
+
+#np.fft.fft()
+SAMPLE_FREQ = 30
+SAMPLE_PERIOD = 1/SAMPLE_FREQ
+SAMPLE_COUNT = 887
+freqAxStep = SAMPLE_FREQ/SAMPLE_COUNT
+freq = np.arange(0, SAMPLE_FREQ/2 + freqAxStep, freqAxStep)
+
+red = np.zeros(SAMPLE_COUNT)
+green = np.zeros(SAMPLE_COUNT)
+blue = np.zeros(SAMPLE_COUNT)
+
+x = np.arange(0, SAMPLE_COUNT)
+
+
+
+with open('24feb_data', 'r') as file:
+    i = 0
+    for line in file:
+        # Split the line into components
+        parts = line.strip().split()
+        if len(parts) == 3:
+            # Convert each part to float and append to respective array
+            red[i] = (float(parts[0]))
+            green[i] = (float(parts[1]))
+            blue[i] = (float(parts[2]))
+            i += 1
+
+
+red -= np.average(red)
+green -= np.average(green)
+blue -= np.average(blue)
+
+red = butter_bandpass_filter(red, 0.5, 5, SAMPLE_FREQ)
+green = butter_bandpass_filter(green, 0.5, 5, SAMPLE_FREQ)
+blue = butter_bandpass_filter(blue, 0.5, 5, SAMPLE_FREQ)
+
+red_fft_y = np.fft.rfft(red)
+red_fft_x = freq
+red_fft_ax.plot(red_fft_x[1:], np.abs(red_fft_y), color = 'red')
+
+green_fft_y = np.fft.rfft(green)
+green_fft_x = freq
+green_fft_ax.plot(green_fft_x[1:], np.abs(green_fft_y), color = 'green')
+
+blue_fft_y = np.fft.rfft(blue)
+blue_fft_x = freq
+blue_fft_ax.plot(blue_fft_x[1:], np.abs(blue_fft_y), color = 'blue')
+
+red_ax.plot(x, red, color = 'red')
+green_ax.plot(x, green, color = 'green')
+blue_ax.plot(x, blue, color = 'blue')
+
+print(freq[np.argmax(np.abs(red_fft_y))])
+
+plt.show()
+
+#x = np.linspace(0,10000)
+#sig = np.sin(1*x)
+#print(SNR(sig, 2000, 10000))
